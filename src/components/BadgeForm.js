20349ecf74fc09abeacbe818bd54029163d7ad86
Erick <erickson.998070@gmail.com>
2020-02-03 20:19:38 -0600

    Enlazamiento de eventos

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,8 +1,43 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  handleChange(e) {
+    console.log({
+      name: e.target.name,
+      value: e.target.value
+    });
+  }
+
+  handleClick(e) {
+    console.log("Botton was clicked");
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("form was sumitted");
+  }
+
   render() {
-    return <p> BadgeForm</p>;
+    return (
+      <div>
+        <h1>New Attendant</h1>
+
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label> First Name</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="firtsName"
+            />
+          </div>
+
+          <button onClick={this.handleClick} className="botton btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
